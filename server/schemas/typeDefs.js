8f08090f9acb46c082dd5950bd51c0269f502b35
friel2021 <friel2009@gmail.com>
2021-10-04 16:00:42 -0600

    removed routes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,17 +2,17 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   type Book {
-    bookId: String
     authors: [String]
     description: String
-    title: String
+    bookId: String!
     image: String
     link: String
+    title: String!
   }
   type User {
     _id: ID
-    username: String
-    email: String
+    username: String!
+    email: String!
     bookCount: Int
     savedBooks: [Book]
   }
@@ -20,22 +20,22 @@ const typeDefs = gql`
     token: ID!
     user: User
   }
+  input SavedBookInput {
+    authors: [String]
+    title: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+  }
   type Query {
     me: User
-    users: [User]
   }
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(
-      authors: [String]
-      description: String
-      title: String
-      bookId: String
-      image: String
-      link: String
-    ): User
-    removeBook(bookId: String): User
+    saveBook(input: SavedBookInput): User
+    removeBook(bookId: String!): User
   }
 `;
 
--- server/server.js
+++ server/server.js
@@ -1,30 +1,26 @@
 const express = require("express");
 const path = require("path");
-// import ApolloServer
+const db = require("./config/connection");
 const { ApolloServer } = require("apollo-server-express");
 
-// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
-const db = require("./config/connection");
-
 const { authMiddleware } = require("./utils/auth");
 
-const PORT = process.env.PORT || 3001;
 const app = express();
-// create a new Apollo server and pass in our schema data
+const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-// integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
