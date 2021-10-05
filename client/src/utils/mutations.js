7b81cea47605ceddb7adf2abcc096eff5812825c
friel2021 <friel2009@gmail.com>
2021-10-04 19:32:54 -0600

    updated

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,18 +25,18 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($book: SavedBookInput!) {
-    saveBook(book: $book) {
+  mutation saveBook($input: BookInput!) {
+    saveBook(input: $input) {
+      _id
       username
       email
-      bookCount
       savedBooks {
-        authors
-        description
         bookId
+        authors
+        title
+        description
         image
         link
-        title
       }
     }
   }
@@ -45,16 +45,11 @@ export const SAVE_BOOK = gql`
 export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
+      _id
       username
-      email
-      bookCount
       savedBooks {
-        authors
-        description
-        bookId
-        image
-        link
         title
+        authors
       }
     }
   }
